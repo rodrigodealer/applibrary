d3640ccb3c5f5c9252c6a94cd68ecdd14ad4fad7
Rodrigo Oliveira <rol@tradeshift.com>
2016-02-17 18:57:20 +0100

    Adding PingController tests

---
+++ src/test/scala/com/github/rodrigodealer/controller/PingControllerTest.scala
@@ -0,0 +1,19 @@
+package com.github.rodrigodealer.controller
+
+import com.github.rodrigodealer.Main
+import com.twitter.finagle.http.Status
+import com.twitter.finatra.http.test.EmbeddedHttpServer
+import com.twitter.inject.app.FeatureTest
+
+class PingControllerTest extends FeatureTest {
+   override val app = new EmbeddedHttpServer(new Main)
+
+  "ping feature" should {
+
+    "respond accordingly" in {
+      app.httpGet(
+        path = "/ping",
+        andExpect = Status.Ok)
+    }
+  }
+}
