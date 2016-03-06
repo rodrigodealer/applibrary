97b9cbb20205613070b692fe00b4017c026df5d2
Rodrigo Oliveira <rol@tradeshift.com>
2016-03-06 11:11:55 +0100

    Cleanup

--- src/main/scala/com/ts/docs/Storage.scala
+++ src/main/scala/com/ts/docs/Storage.scala
@@ -3,9 +3,6 @@ package com.ts.docs
 import com.google.inject.Inject
 import com.ts.docs.IOUtils._
 import com.twitter.finagle.builder.ClientBuilder
-import com.twitter.finagle.http.Method.Post
-import com.twitter.finagle.http.Version.Http11
-import com.twitter.finagle.http.{Http, Request}
 import com.twitter.finagle.redis.util.StringToChannelBuffer
 import com.twitter.finagle.redis.{Redis, TransactionalClient}
 import com.twitter.finagle.stats.StatsReceiver
@@ -59,29 +56,6 @@ object IOUtils {
 trait Storage {
   def set(record: Record): Future[Boolean]
   def add(record: Record): Future[Boolean]
-}
-
-class RiakStorage(bucketName: String) extends Storage {
-
-  // private val bucket = RiakClient("localhost", 8098).bucket(bucketName)
-
-  // the default client is slow. So we use the raw http api so we can stream the file directly.
-  val client = // Http.client.withStreaming(true).newService("localhost:8098")
-    ClientBuilder()
-      .codec(new Http())
-      .hosts("localhost:8098")
-      .hostConnectionLimit(100)
-      .keepAlive(true)
-      .build()
-
-  def set(record: Record) = {
-    val newReq = Request(Http11, Post, s"//buckets/$bucketName/keys", record.value)
-    newReq.setContentType(mediaType = "text/plain")
-    newReq.setChunked(true)
-    client(newReq) map (_.getStatusCode() == 201)
-  }
-
-  override def add(record: Record): Future[Boolean] = ???
 }
 
 case class Record(key: String, value: Reader)
