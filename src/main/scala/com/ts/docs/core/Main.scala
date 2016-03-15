6ded90448809459c62526d31822b7a2663d9049c
André Gonçalves <andre.rigon@gmail.com>
2016-03-15 19:12:09 +0100

    tsuru config

--- src/main/scala/com/ts/docs/core/Main.scala
+++ src/main/scala/com/ts/docs/core/Main.scala
@@ -6,41 +6,6 @@ import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 
-/*
-object Finagle {
-
-  def createStorage(s: Option[String]) = s.filterNot(_.isEmpty) match {
-    case Some("redis") =>
-      println("Using Redis storage")
-      new RedisStorage()
-    case _ =>
-      println("using Riak storage")
-      new RiakStorage("mybucket")
-  }
-
-  def service(storage: Storage) = new Service[http.Request, http.Response] {
-    def apply(req: http.Request): Future[http.Response] = {
-      storage.set(Record("" + System.nanoTime(), req.reader)) map { x =>
-        req.response
-      }
-    }
-  }
-
-  def writeBytes( data : Stream[Byte], file : File ) = {
-    val target = new BufferedOutputStream( new FileOutputStream(file) )
-    try data.foreach( target.write(_) ) finally target.close()
-  }
-
-
-  def main(args: Array[String]) {
-    val storage = createStorage(args.toList.headOption)
-    val server = Http.server.withStreaming(true).serve(":8080", service(storage))
-    Await.ready(server)
-  }
-
-}
-*/
-
 object Finatra extends Main {}
 
 class Main extends HttpServer {
